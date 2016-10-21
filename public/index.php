9e411f4a468d4e5a079ab3931070bc3a22dc8f30
Ju/TroisYaourts <julien@troisyaourts.com>
2016-10-21 15:22:36 +0200

    webhook subscribe

--- public/index.php
+++ public/index.php
@@ -4,6 +4,14 @@ require_once("../vendor/autoload.php");
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+
+// CONSTANTES
+
+define('APP_ID', get_env('APP_ID'));
+define('APP_SECRET', get_env('APP_SECRET'));
+define('PAGE_ACCESS_TOKEN', get_env('PAGE_ACCESS_TOKEN'));
+define('VALIDATION_TOKEN', get_env('VALIDATION_TOKEN'));
+
 //BOOTSTRAPING
 
 $app = new Silex\Application();
@@ -18,6 +26,14 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 //CONTROLLERS
 
 $app->get('/webhook', function(Request $request) use ($app){
+    //?hup_mode=subscribe&hub_verify_token=valeur_secret&hub_challenge==1234566
+    if(   $request->query->get('hub_mode') === 'subscribe'
+       && $request->query->get('hub_verify_token') === VALIDATION_TOKEN){
+      return new Response($request->query->get('hub_challenge') );
+    }else{
+      return new Response("SUBSCRIBE_FAILED");
+    }
+
     return new Response("ça marche !");
 });
 
